c50a4c9b627400349985a19b5e19eee79be69368
Brooke Adams <>
2022-05-23 22:07:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+name = input("Kia Ora, what is your name? ")
+print ("Kia Ora", name+ "!")
+
+import question1_test
+import question2_test
---
+++ question1_test.py
@@ -0,0 +1,4 @@
+print("lets start! question one.")
+
+def question1():
+  print("")
